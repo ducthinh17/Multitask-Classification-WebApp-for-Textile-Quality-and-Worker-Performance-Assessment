6950a8aeae53ebf30d4ec0c7f443e5ef4f4a274f
ducthinh17 <pldthinh.ityu@gmail.com>
2025-05-20 11:59:22 +0700

    update key

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
 // src/config.ts
 // QUAN TRỌNG: Thay thế bằng URL Ngrok hiện tại của bạn nếu nó đã thay đổi
-export const PREDICT_API_URL = "https://86ac-35-230-164-30.ngrok-free.app/predict";
+export const PREDICT_API_URL = "https://egret-touching-unlikely.ngrok-free.app/predict";
